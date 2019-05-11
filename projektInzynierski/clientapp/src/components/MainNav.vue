32891e5331319dd6d813b9f639b4c7b5c7f028ff
Kamil Bania <kamilbania2@gmail.com>
2019-05-12 01:07:27 +0200

    Utworzenie komponentu MainNav

---
+++ projektInzynierski/clientapp/src/components/MainNav.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <ul>
+            <li><router-link to="/login">Logowanie</router-link></li>
+            <li><router-link to ="/register">Rejestracja</router-link></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+
+export default {
+
+}
+
+</script>
+
+<style scoped>
+
+</style>
