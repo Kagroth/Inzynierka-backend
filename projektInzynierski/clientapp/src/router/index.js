0a9d5854a551b21f965671c4ffbc052c43a8a031
Kamil Bania <kamilbania2@gmail.com>
2019-05-12 01:07:56 +0200

    Dodanie tras do Login i Register form

--- projektInzynierski/clientapp/src/router/index.js
+++ projektInzynierski/clientapp/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import MainNav from '@/components/MainNav'
+import LoginForm from '@/components/LoginForm'
+import RegisterForm from '@/components/RegisterForm'
 
 Vue.use(Router)
 
@@ -8,8 +10,17 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'App',
+      component: MainNav
+    },
+    {
+      path: '/login',
+      component: LoginForm
+    },
+    {
+      path: '/register',
+      component: RegisterForm
     }
-  ]
+  ],
+  mode: 'history'
 })
