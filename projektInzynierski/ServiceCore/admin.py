6433e92a174e861c87902e0518b564a293ed6fcd
Kamil Bania <kamilbania2@gmail.com>
2019-05-11 18:01:41 +0200

    Dodanie modeli do panelu administracyjnego Django

--- projektInzynierski/ServiceCore/admin.py
+++ projektInzynierski/ServiceCore/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from ServiceCore.models import *
 
 # Register your models here.
+# Modele w panelu administratora
+admin.site.register(UserType)
+admin.site.register(Profile)
+admin.site.register(Exercise)
+admin.site.register(UnitTest)
+admin.site.register(Test)
+admin.site.register(TaskType)
+admin.site.register(Task)
+admin.site.register(Group)
+admin.site.register(Solution)
