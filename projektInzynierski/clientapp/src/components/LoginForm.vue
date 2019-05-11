fe4a67c0257ab7d5808e11850e436765c333b86a
Kamil Bania <kamilbania2@gmail.com>
2019-05-12 01:07:04 +0200

    Utworzenie komponentu LoginForm

---
+++ projektInzynierski/clientapp/src/components/LoginForm.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <router-link to="/">Strona glowna</router-link><br><br>
+        <form>
+            <input type="text" placeholder="login"/><br>
+            <input type="password" placeholder="haslo"/><br>
+            <input type="submit" value="Zaloguj"/>
+        </form>
+    </div>
+</template>
+
+<script>
+
+export default {
+
+}
+
+</script>
+
+<style scoped>
+
+</style>
