75993eec6550112cb967f5cc93ff7260f83d9007
Kamil Bania <kamilbania2@gmail.com>
2019-05-12 01:07:37 +0200

    Utworzenie komponentu RegisterForm

---
+++ projektInzynierski/clientapp/src/components/RegisterForm.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <router-link to="/">Strona glowna</router-link>
+    <br>
+    <br>
+    <form>
+      <input type="text" placeholder="Imie">
+      <br>
+      <input type="text" placeholder="Nazwisko">
+      <br>
+      <input type="text" placeholder="Nazwa uzytkownika" v-model="form.username">
+      <br>
+      <input type="email" placeholder="email">
+      <br>
+      <input type="password" placeholder="Haslo">
+      <br>
+      <input type="password" placeholder="Powtorz haslo">
+      <br>
+      <input type="submit" value="Zarejestruj" @click="registerUser">
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        username: ""
+      }
+    };
+  },
+
+  methods: {
+    registerUser(event) {
+      event.preventDefault();
+      console.log("Username in component: " + this.form.username);
+      this.$store.dispatch("createUser", {username: this.form.username});
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
