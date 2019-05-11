251ce9600e77188610e52d5a44a08dcc82eb92b8
Kamil Bania <kamilbania2@gmail.com>
2019-05-11 18:03:20 +0200

    Utworzenie widoku zwracajacego wszystkich userow

--- projektInzynierski/ServiceCore/views.py
+++ projektInzynierski/ServiceCore/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse
 
+from django.contrib.auth.models import User
+
+from ServiceCore.serializers import UserSerializer
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+from rest_framework.renderers import JSONRenderer
+
 # Create your views here.
 def index(request):
     return HttpResponse("Test")
+
+
+class ListUsers(APIView):
+    permission_classes = (AllowAny,)
+
+    def get(self, request):
+        usersSet = User.objects.all()
+        # tlumaczenie obiektow na dict
+        usersSetSerialized = UserSerializer(usersSet, many=True)
+        # konwersja dict na json
+        #usersSetJson = JSONRenderer().render(usersSetSerialized.data)
+
+        return Response(usersSetSerialized.data)
