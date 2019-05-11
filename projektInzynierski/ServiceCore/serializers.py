6d8af2482d17b307d9c358b4787f27e51dd72421
Kamil Bania <kamilbania2@gmail.com>
2019-05-11 18:12:39 +0200

    Utworzenie GroupSerializer

--- projektInzynierski/ServiceCore/serializers.py
+++ projektInzynierski/ServiceCore/serializers.py
@@ -1,9 +1,16 @@
 
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from ServiceCore.models import Group
 
 # User serializer model
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('username', 'email', 'first_name', 'last_name')
+    
+# ServiceCore Group serializer model
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Group
+        fields = ('name')
