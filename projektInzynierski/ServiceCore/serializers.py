6785b7393121ee83713acb9c97f323311cd6027a
Kamil Bania <kamilbania2@gmail.com>
2019-05-11 19:29:23 +0200

    Utworzenie serializerow UserType i Profile

--- projektInzynierski/ServiceCore/serializers.py
+++ projektInzynierski/ServiceCore/serializers.py
@@ -1,21 +1,36 @@
 
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from ServiceCore.models import Group
+from ServiceCore.models import Group, Profile, UserType
 
-# User serializer model
+# UserType model serializer
+class UserTypeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserType
+        fields = ('name',)
+
+# Profile model serializer
+class ProfileSerializer(serializers.ModelSerializer):
+    userType = UserTypeSerializer()
+
+    class Meta:
+        model = Profile
+        fields = ('userType',)
+
+# User model serializer
 class UserSerializer(serializers.ModelSerializer):
+    profile = ProfileSerializer()
+
     class Meta:
         model = User
-        fields = ('username', 'email', 'first_name', 'last_name')
+        fields = ('username', 'email', 'first_name', 'last_name', 'profile')
     
-# ServiceCore Group serializer model
+# ServiceCore Group model serializer 
 class GroupSerializer(serializers.ModelSerializer):
     # SlugRelatedField pozwala na wypisanie polaczonych obiektow w postaci pola slug_field
     # users = serializers.SlugRelatedField(many=True, read_only=True, slug_field='username')
     
     # Nested Serializer pozwala na wypisanie obiektow w postaci zserializowanej
-    
     users = UserSerializer(many=True)
 
     class Meta:
