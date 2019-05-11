946fda1e6644f0cb0509cc5c2a4f81a06e236d98
Kamil Bania <kamilbania2@gmail.com>
2019-05-11 18:48:47 +0200

    Dodanie path to uzyskania tokena/logowania

--- projektInzynierski/ServiceCore/urls.py
+++ projektInzynierski/ServiceCore/urls.py
@@ -2,7 +2,14 @@
 from . import views
 from django.urls import path
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 urlpatterns = [
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_obtain_pair'),
     path('', views.index),
     path('users/', views.ListUsers.as_view()),
     path('groups/', views.ListGroup.as_view()),
