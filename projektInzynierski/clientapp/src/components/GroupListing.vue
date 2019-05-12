846f1234b777eda615510c682dfbe6c836022ce7
Kamil Bania <kamilbania2@gmail.com>
2019-05-13 00:27:50 +0200

    Komponent z listingiem grup

---
+++ projektInzynierski/clientapp/src/components/GroupListing.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <group :key="group" v-for="group in groups" v-bind:group="group"/>
+  </div>
+</template>
+
+<script>
+import Group from '@/components/Group'
+
+
+export default {
+    components: {
+      'group': Group
+    },
+
+    data() {
+        return {
+        }
+    },
+
+    methods: {
+
+    },
+
+    computed: {
+      groups() {
+        return this.$store.state.groups
+      }
+    },
+
+    created() {
+      this.$store.dispatch('getAllGroups').then(
+        () => { 
+          alert("Wszystkie grupy zwrocone!")
+          this.groups = this.$store.state.groups
+          }
+      )
+      .catch(
+        () => { alert("Nie udalo sie zwrocic grup!")}
+      )
+    }
+    
+}
+</script>
+
+<style scoped>
+
+</style>
