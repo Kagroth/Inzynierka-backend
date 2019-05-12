b7ce44d0021ab9a481290248eded08bc2631244e
Kamil Bania <kamilbania2@gmail.com>
2019-05-13 00:27:36 +0200

    Utworzenie komponentu reprezentujacego Groupe

---
+++ projektInzynierski/clientapp/src/components/Group.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h3>{{group.name}}</h3>
+    <ol>
+      <li :key="user" v-for="user in group.users">
+        <p>Imie i nazwisko: {{user.first_name}} {{user.last_name}}</p>
+        <p>Email: {{user.email}}</p>
+        <p>Nazwa użytkownika: {{user.username}}</p>
+      </li>
+    </ol>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['group'],
+  
+  data() {
+    return {
+    };
+  },
+
+  methods: {}
+};
+</script>
+
+<style scoped>
+</style>
