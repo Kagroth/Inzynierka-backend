691732e0e3b6c4b91e161f62c5667c20e1056438
Kamil Bania <kamilbania2@gmail.com>
2019-05-12 23:12:38 +0200

    Utworzenie obiektu form i metody loginUser

--- projektInzynierski/clientapp/src/components/LoginForm.vue
+++ projektInzynierski/clientapp/src/components/LoginForm.vue
@@ -2,9 +2,9 @@
     <div>
         <router-link to="/">Strona glowna</router-link><br><br>
         <form>
-            <input type="text" placeholder="login"/><br>
-            <input type="password" placeholder="haslo"/><br>
-            <input type="submit" value="Zaloguj"/>
+            <input type="text" placeholder="Nazwa uzytkownika" v-model="form.username"/><br>
+            <input type="password" placeholder="haslo" v-model="form.password"/><br>
+            <input type="submit" value="Zaloguj" @click="loginUser"/>
         </form>
     </div>
 </template>
@@ -12,7 +12,23 @@
 <script>
 
 export default {
+    data() {
+        return {
+            form: {
+                username: "",
+                password: ""
 
+            }   
+        }
+    },
+
+    methods: {
+        loginUser(event) {
+            event.preventDefault();
+            
+            this.$store.dispatch('loginUser', this.form);
+        }
+    }
 }
 
 </script>
