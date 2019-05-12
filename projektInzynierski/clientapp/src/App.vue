84db5ae78645798b5c8f321dc64276315122d1d0
Kamil Bania <kamilbania2@gmail.com>
2019-05-13 00:49:26 +0200

    WYciagniecie MainNav poza router-view

--- projektInzynierski/clientapp/src/App.vue
+++ projektInzynierski/clientapp/src/App.vue
@@ -1,11 +1,17 @@
 <template>
   <div id="app">
+    <main-nav/>
     <router-view/>
   </div>
 </template>
 
 <script>
+import MainNav from '@/components/MainNav'
 export default {
+  components: {
+    'main-nav': MainNav
+  },
+
   name: 'App'
 }
 </script>
