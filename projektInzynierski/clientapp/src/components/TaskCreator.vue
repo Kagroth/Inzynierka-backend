65339eda6b79007fa6b696a5c99eec016a7416ff
Kamil Bania <kamilbania2@gmail.com>
2019-06-02 13:04:09 +0200

    Utworzenie komponentów związanych z Taskami

---
+++ projektInzynierski/clientapp/src/components/TaskCreator.vue
@@ -0,0 +1,40 @@
+<template>   
+    <div>
+        <h3>Kreator zadania</h3>
+        <form>
+            
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            form: {
+            }
+        }
+    },
+
+    methods: {
+        createTask(event) {
+            event.preventDefault();
+            
+            for(let field in this.form) {
+                if(this.form[field] === "") {
+                    alert("Nie podano wszystkich danych!")
+                    return
+                }
+            }
+
+            console.log(this.form)
+
+            this.$store.dispatch('createExercise', this.form).then((responseData) => {
+                alert(responseData.message)
+            })
+        }
+    }
+}
+</script>
+
+<style scoped>
