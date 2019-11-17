7b60b19d01002c5807ea9c469f744f7a660af0d5
Kamil Bania <kamilbania2@gmail.com>
2019-11-18 00:26:49 +0100

    Add paths to Level and Language endpoints

--- projektInzynierski/ServiceCore/urls.py
+++ projektInzynierski/ServiceCore/urls.py
@@ -19,6 +19,8 @@ urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_obtain_pair'),
     path('profile/<str:username>', views.ProfileView.as_view()),
+    path('levels/', views.LevelView.as_view()),
+    path('languages/', views.LanguageView.as_view()),
     path('', include(router.urls)),
     #path('users/', views.ListUsers.as_view()),
     #path('groups/', views.ListGroup.as_view()),
