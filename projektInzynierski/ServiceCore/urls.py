ad670d2ca0e587e3e4e2facbcfb7688929ec6f60
Kamil Bania <kamilbania2@gmail.com>
2019-11-26 00:46:01 +0100

    Refresh token name path change

--- projektInzynierski/ServiceCore/urls.py
+++ projektInzynierski/ServiceCore/urls.py
@@ -18,7 +18,7 @@ router.register('tasks', views.TaskViewSet, basename="Tasks")
 
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_obtain_refresh'),
     path('profile/<str:username>', views.ProfileView.as_view()),
     path('levels/', views.LevelView.as_view()),
     path('languages/', views.LanguageView.as_view()),
