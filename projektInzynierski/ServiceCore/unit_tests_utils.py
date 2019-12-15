3002a20583f0966cf9210b8d78fb731df29f49e0
Kamil Bania <kamilbania2@gmail.com>
2019-12-15 16:32:42 +0100

    Write unit tests assertions to file fix

--- projektInzynierski/ServiceCore/unit_tests_utils.py
+++ projektInzynierski/ServiceCore/unit_tests_utils.py
@@ -12,12 +12,13 @@ def create_unit_tests(exercise, unit_tests_data):
             unit_test_file.write("import unittest \n\
 import sys \n\
 from solution import * \n\n\
-class FirstTest(unittest.TestCase):\n \
-    def test_first(self):\n\
-        " + unit_test + "\n\n\
-if __name__ == '__main__':\n\
-    unittest.main()")
-
+class FirstTest(unittest.TestCase):\n\t\
+def test_first(self):\n")
+            for line in unit_test.split("\n"):
+                print(line)
+                unit_test_file.write("\t\t" + line + "\n")
+            
+            unit_test_file.write("\nif __name__ == '__main__':\n\tunittest.main()")
 
         # zapis sciezki w modelu nie dziala
         print(pathToFile)
