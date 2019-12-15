1d512e0632902890b4b532064e7704891df5564a
Kamil Bania <kamilbania2@gmail.com>
2019-12-15 20:05:05 +0100

    Refactoring of all functions which creates dirs

--- projektInzynierski/ServiceCore/utils.py
+++ projektInzynierski/ServiceCore/utils.py
@@ -5,6 +5,194 @@ EXERCISES_DIRECTORY_ROOT = "exercises"
 TESTS_DIRECTORY_ROOT = "exercises_tests"
 SOLUTIONS_DIRECTORY_ROOT = "solutions"
 
+def createDirectory(pathToCreate):
+    # funkcja tworzy folder w lokalizacji path/dirName
+    if not os.path.exists(pathToCreate):
+        os.mkdir(pathToCreate)
+        return True
+    else:
+        return False
+
+# ************ funkcje dla cwiczen - Exercises *******************
+def getExerciseDirectoryName(exercise):
+    # funkcja zwraca nazwe folderu odpowiednia dla podanego cwiczenia
+    return exercise.title.replace(" ", "") + '-' + exercise.author.username.replace(" ", "") + '-' + str(exercise.pk)
+
+def getExerciseDirectoryRootPath(exercise):
+    # funkcja zwraca sciezke do folderu z konkretnym cwiczeniem w ktorym sa skladowane unit testy
+    directoryName = getExerciseDirectoryName(exercise)
+    cwd = os.getcwd()
+    
+    return os.path.join(cwd, EXERCISES_DIRECTORY_ROOT, directoryName)
+
+def createExerciseRootDirectory(exercise):
+    # funkcja tworzy folder dla konkretnego cwiczenia w folderze glownym dla cwiczen
+    directoryName = getExerciseDirectoryName(exercise)
+    cwd = os.getcwd()
+    
+    pathToExercise = os.path.join(cwd, EXERCISES_DIRECTORY_ROOT, directoryName)
+
+    return createDirectory(pathToExercise)
+
+def createExerciseDirectory(exercise, relPath):
+    # funkcja tworzy folder dla cwiczenia exercise w lokalizacji relPath
+    directoryName = getExerciseDirectoryName(exercise)
+    cwd = os.getcwd()
+
+    pathToExercise = os.path.join(cwd, relPath, directoryName)
+
+    return createDirectory(pathToExercise)
+
+
+# ************ funkcje dla kolokwium - Tests *********************
+def getTestDirectoryName(test):
+    # funckja zwraca nazwe folderu dla podanego kolokwium
+    return test.title.replace(" ", "") + '-' + test.author.username.replace(" ", "") + '-' + str(test.pk)
+
+def getTestDirectoryRootPath(test):
+    # Zwraca lokalizacje w ktorej zapisane jest kolokwium
+    directoryName = getTestDirectoryName(test)
+    cwd = os.getcwd()
+    
+    return os.path.join(cwd, TESTS_DIRECTORY_ROOT, directoryName)
+
+
+def createTestRootDirectory(test):
+    # funkcja tworzy folder dla kolokwium 'test' w folderze root dla kolokwium
+    directoryName = getTestDirectoryName(test)
+    cwd = os.getcwd()
+    pathToTest = os.path.join(cwd, TESTS_DIRECTORY_ROOT, directoryName)
+
+    created = createDirectory(pathToTest)
+
+    if created:
+        for exercise in test.exercises.all():
+            if createExerciseDirectory(exercise, pathToTest):
+                exerciseRootPath = getExerciseDirectoryRootPath(exercise)
+                exerciseDirName = getExerciseDirectoryName(exercise)
+                exerciseInTestDirPath = os.path.join(pathToTest, exerciseDirName)
+
+                copy_tree(exerciseRootPath, exerciseInTestDirPath)
+            else:
+                return False        
+
+        return True
+    else:
+        return False
+
+def createTestDirectory(test, relPath):
+    # funkcja tworzy folder dla kolokwium 'test' w lokalizacji relPath
+    directoryName = getTestDirectoryName(test)
+    cwd = os.getcwd()
+    pathToTest = os.path.join(cwd, relPath, directoryName)
+
+    created = createDirectory(pathToTest)
+
+    if created:
+        for exercise in test.exercises.all():
+            if createExerciseDirectory(exercise, pathToTest):
+                exerciseRootPath = getExerciseDirectoryRootPath(exercise)
+                exerciseDirName = getExerciseDirectoryName(exercise)
+                exerciseInTestDirPath = os.path.join(pathToTest, exerciseDirName)
+
+                copy_tree(exerciseRootPath, exerciseInTestDirPath)
+            else:
+                return False        
+
+        return True
+    else:
+        return False
+
+# *************funkcje dla rozwiazan zadan - Tasks and Solutions *****************
+def getTaskSolutionsDirectoryName(task):
+    # funkcja zwraca nazwe folderu w ktorym znajduja sie rozwiazania zadania 'task'
+    return task.title.replace(" ", "") + '-' + task.author.username.replace(" ", "") + '-' + str(task.pk)    
+
+
+def createExerciseSolutionDirectory(task):
+    # funkcja tworzy strukture katalogow dla zadania typu exercise
+    # - RootDir Solutions
+    #   - Task dir
+    #       - GroupA dir
+    #           - UserA solution
+    #           - UserB solution
+    #       - GroupB dir
+    #           - UserC solution
+    directoryName = getTaskSolutionsDirectoryName(task)
+
+    cwd = os.getcwd()
+    pathToSolution = os.path.join(cwd, SOLUTIONS_DIRECTORY_ROOT, directoryName)
+    
+    if not createDirectory(pathToSolution):
+        return False
+    
+    if not createExerciseDirectory(task.exercise.get(), pathToSolution):
+        return False
+
+    for group in task.assignedTo.all():
+        groupName = group.name + '-' + str(group.pk)
+        pathToAssignedGroupSolution = os.path.join(pathToSolution, getExerciseDirectoryName(task.exercise.get()), groupName)
+        created = createDirectory(pathToAssignedGroupSolution)
+
+        if not created:
+            return False
+        
+        for member in group.users.all():
+            memberName = member.username.replace(" ", "") + '-' + str(member.pk)
+            pathToGroupMemberSolution = os.path.join(pathToAssignedGroupSolution, memberName)
+
+            created = createDirectory(pathToGroupMemberSolution)
+
+            if not created:
+                return False
+
+    return True
+
+
+def createTestSolutionDirectory(task):
+    # funkcja tworzy katalog z rozwiazaniem dla kolokwium w postaci:
+    # - RootDir Solutions
+    #   - Task dir
+    #       - GroupA
+    #           - UserA dir
+    #               - ExerciseA dir
+    #               - ExerciseB dir 
+    directoryName = getTaskSolutionsDirectoryName(task)
+    cwd = os.getcwd()
+    pathToSolution = os.path.join(cwd, SOLUTIONS_DIRECTORY_ROOT, directoryName)
+
+    if not createDirectory(pathToSolution):
+        return False
+
+    if not createTestDirectory(task.test.get(), pathToSolution):
+        return False
+
+    for group in task.assignedTo.all():
+        groupName = group.name + '-' + str(group.pk)
+        pathToAssignedGroupSolution = os.path.join(pathToSolution, getExerciseDirectoryName(task.exercise.get()), groupName)
+        created = createDirectory(pathToAssignedGroupSolution)
+
+        if not created:
+            return False
+        
+        for member in group.users.all():
+            memberName = member.username.replace(" ", "") + '-' + str(member.pk)
+            pathToGroupMemberSolution = os.path.join(pathToAssignedGroupSolution, memberName)
+
+            if not createDirectory(pathToGroupMemberSolution):
+                return False
+
+            for exercise in task.test.exercises.all():
+                exerciseDirName = getExerciseDirectoryName(exercise)
+                exerciseInTestPath = os.path.join(pathToGroupMemberSolution, exerciseDirName)
+
+                if not createDirectory(exerciseInTestPath):
+                    return False
+
+    return True
+
+
+
 # funkcja dla kazdego uzytkownika w kazdej grupie tworzy folder w ktorym bedzie 
 # przechowywane rozwiazanie zadania konkretnego uzytkownika
 def createSubdirectoryForUsersInGroup(group, groupSolutionsPath):
@@ -57,24 +245,9 @@ def createSubdirectoryForAssignedGroup(task, solutionPath):
         True
     )
 
-
-# funkcja tworzy folder ktory bedzie przechowywal rozwiazania zadania 'task'
-def createDirectoryForTaskSolutions(task):
-    typeOfTask = task.taskType.name
-
-    if typeOfTask == "Test":
-        # utworzenie katalogow dla kolokwium
-        pass
-    elif typeOfTask == "Exercise":
-        # utworzenie katalogu dla exercise
-        pass
-    else:
-        return (
-            "Podano niepoprawny rodzaj zadania",
-            False
-        )
-
-    directoryName = task.title.replace(" ", "") + '-' + task.author.username.replace(" ", "") + '-' + str(task.pk)
+'''funkcja tworzy folder z rozwiazaniami dla zadania typu Exercise
+def createExerciseSolutionDirectory(task):
+    directoryName = getTaskSolutionsDirectoryName(task)
 
     cwd = os.getcwd()
     pathToTaskSolutions = os.path.join(cwd, SOLUTIONS_DIRECTORY_ROOT, directoryName)
@@ -82,7 +255,7 @@ def createDirectoryForTaskSolutions(task):
     if not os.path.exists(pathToTaskSolutions):
         os.mkdir(pathToTaskSolutions)
     else:
-        print("Utworzono zadanie ale nie udalo sie utworzyc folderu")
+        print("Nie udalo sie utworzyc folderu")
         return (
             "Folder o podanej nazwie juz istnieje",
             False
@@ -93,73 +266,36 @@ def createDirectoryForTaskSolutions(task):
     print(result)
 
 
-# funkcja tworzy folder dla konkretnego cwiczenia
-def createExerciseDirectory(exercise):
-    directoryName = getExerciseDirectoryName(exercise)
+def createTestSolutionDirectory(task):
+    directoryName = getTaskSolutionsDirectoryName(task)
     cwd = os.getcwd()
-    pathToExercise = os.path.join(cwd, EXERCISES_DIRECTORY_ROOT, directoryName)
+    pathToTaskSolutions = os.path.join(cwd, SOLUTIONS_DIRECTORY_ROOT, directoryName)
 
-    if not os.path.exists(pathToExercise):
-        os.mkdir(pathToExercise)
-        return (
-            "Utworzono folder dla cwiczenia",
-            True
-        )
+    for group in task.assignedTo.all():
+        groupName = group.name.replace(" ", "") + '-' + str(group.pk)
+        pathToGroupSolutions = os.path.join(pathToTaskSolutions, groupName)
+
+
+        
+    for exercise in task.test.exercises.all():
+        createExerciseDirectory(exercise, pathToTaskSolutions)
+'''
+
+# funkcja tworzy folder ktory bedzie przechowywal rozwiazania zadania 'task'
+def createDirectoryForTaskSolutions(task):
+    typeOfTask = task.taskType.name
+
+    if typeOfTask == "Test":
+        createTestSolutionDirectory(task)
+    elif typeOfTask == "Exercise":
+        createExerciseSolutionDirectory(task)
     else:
-        print()
         return (
-            "Nie udalo sie utworzyc folderu dla tego cwiczenia",
+            "Podano niepoprawny rodzaj zadania",
             False
         )
 
-# funkcja tworzy folder dla kolokwium 'test'
-def createTestDirectory(test):
-    directoryName = getTestDirectoryName(test)
-    cwd = os.getcwd()
-    pathToTest = os.path.join(cwd, TESTS_DIRECTORY_ROOT, directoryName)
+    return True
 
-    if not os.path.exists(pathToTest):
-        os.mkdir(pathToTest)
 
-        for exercise in test.exercises.all():
-            exercisePath = getExerciseDirectoryPath(exercise)
-            exerciseDirectoryName = getExerciseDirectoryName(exercise)
-            exerciseInTestPath = os.path.join(pathToTest, exerciseDirectoryName)
 
-            copy_tree(exercisePath, exerciseInTestPath)
-
-        return (
-            "Utworzono folder dla kolokwium",
-            True
-        )
-    else:
-        print()
-        return (
-            "Nie udalo sie utworzyc folderu dla tego kolokwium",
-            False
-        )
-
-# funkcja zwraca nazwe folderu odpowiednia dla podanego cwiczenia
-def getExerciseDirectoryName(exercise):
-    return exercise.title.replace(" ", "") + '-' + exercise.author.username.replace(" ", "") + '-' + str(exercise.pk)
-
-# funckaj zwraca nazwe folderu dla podanego kolokwium
-def getTestDirectoryName(test):
-    return test.title.replace(" ", "") + '-' + test.author.username.replace(" ", "") + '-' + str(test.pk)
-
-# funkcja zwraca sciezke do folderu z konkretnym cwiczeniem w ktorym sa skladowane unit testy
-def getExerciseDirectoryPath(exercise):
-    directoryName = getExerciseDirectoryName(exercise)
-    cwd = os.getcwd()
-    
-    return os.path.join(cwd, EXERCISES_DIRECTORY_ROOT, directoryName)
-
-# funkcja zwraca sciezke do rozwiazania zadania przez uzytkownika
-def getUserSolutionPath(task, group, user):
-    directoryName = task.title.replace(" ", "") + '-' + task.author.username.replace(" ", "") + '-' + str(task.pk)
-    groupName = group.name.replace(" ", "") + '-' + str(group.pk)
-    memberName = user.username.replace(" ", "") + '-' + str(user.pk)
-
-    cwd = os.getcwd()
-
-    return os.path.join(cwd, SOLUTIONS_DIRECTORY_ROOT, directoryName, groupName, memberName)
