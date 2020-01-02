843335cf21edff5b0a3099c7bef15371da3c868e
Kamil Bania <kamilbania2@gmail.com>
2020-01-02 20:25:46 +0100

    Create unit tests files in Java exercise directory

--- projektInzynierski/ServiceCore/unit_tests_utils.py
+++ projektInzynierski/ServiceCore/unit_tests_utils.py
@@ -24,14 +24,33 @@ def test_first(self):\n")
             newUnitTest.save()
 
 def create_java_unit_tests(exercise, unit_tests_data):
-    pass
+    for index, unit_test in enumerate(unit_tests_data):
+        print(unit_test)
+        pathToExerciseDir = getExerciseDirectoryRootPath(exercise)
+        pathToUnitTestsDir = os.path.join(pathToExerciseDir, 'src', 'test', 'java')
+        unitTestFileName = "Unit" + str(index) + "Test.java"
+        pathToFile = os.path.join(pathToUnitTestsDir, unitTestFileName)
+
+        with open(pathToFile, "w+") as unit_test_file:
+            unit_test_file.write(
+"import static org.junit.jupiter.api.Assertions.*;\n\
+import org.junit.jupiter.api.Test;\n\
+public class Unit" + str(index) + "Test { \n\
+    @Test \n\
+    public void test1() { \n")
+            for line in unit_test.split("\n"):
+                print(line)
+                unit_test_file.write("\t\t" + line + "\n")
+            unit_test_file.write("\t} \n }")
+            newUnitTest = UnitTest.objects.create(exercise=exercise, pathToFile=pathToFile, content=unit_test)
+            newUnitTest.save()
+
 
 def create_unit_tests(exercise, unit_tests_data):
     if exercise.language.name == 'Python':       
         create_python_unit_tests(exercise, unit_tests_data)
     elif exercise.language.name == 'Java':
-        print("Tu beda twrzone unit testy dla projektu Java")
-        pass
+        create_java_unit_tests(exercise, unit_tests_data)
     else:
         print("Nie da sie utworzyc unit testow dla podanego jezyka")
 
