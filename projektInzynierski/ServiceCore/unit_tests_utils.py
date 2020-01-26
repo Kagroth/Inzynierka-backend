c7fceb31bc409ba0371649d76e49709e9d05ddd5
Kamil Bania <kamilbania2@gmail.com>
2020-01-26 01:25:56 +0100

    Create java exercise unit tests - now one assertion is a single method

--- projektInzynierski/ServiceCore/unit_tests_utils.py
+++ projektInzynierski/ServiceCore/unit_tests_utils.py
@@ -18,31 +18,34 @@ def create_python_unit_tests(exercise, unit_tests_data):
             for index_i, line in enumerate(unit_test.split("\n")):
                 unit_test_file.write("\tdef test_" + str(index_i) + "(self):\n")
                 unit_test_file.write("\t\t" + line + "\n")
-                newUnitTest = UnitTest.objects.create(exercise=exercise, pathToFile=pathToFile, content=unit_test)
-                newUnitTest.save()
+            
+            newUnitTest = UnitTest.objects.create(exercise=exercise, pathToFile=pathToFile, content=unit_test)
+            newUnitTest.save()
             
         unit_test_file.write("\nif __name__ == '__main__':\n\tunittest.main()")
 
 def create_java_unit_tests(exercise, unit_tests_data):
-    for index, unit_test in enumerate(unit_tests_data):
-        print(unit_test)
-        pathToExerciseDir = getExerciseDirectoryRootPath(exercise)
-        pathToUnitTestsDir = os.path.join(pathToExerciseDir, 'src', 'test', 'java')
-        unitTestFileName = "Unit" + str(index) + "Test.java"
-        pathToFile = os.path.join(pathToUnitTestsDir, unitTestFileName)
+    print(unit_tests_data)
+    pathToExerciseDir = getExerciseDirectoryRootPath(exercise)
+    pathToUnitTestsDir = os.path.join(pathToExerciseDir, 'src', 'test', 'java')
+    unitTestFileName = "UnitTest.java"
+    pathToFile = os.path.join(pathToUnitTestsDir, unitTestFileName)
 
-        with open(pathToFile, "w+") as unit_test_file:
-            unit_test_file.write(
-"import static org.junit.jupiter.api.Assertions.*;\n\
-import org.junit.jupiter.api.Test;\n\
-import com.myapp.*;\n\
-public class Unit" + str(index) + "Test { \n\
-    @Test \n\
-    public void test1() { \n")
-            for line in unit_test.split("\n"):
-                print(line)
-                unit_test_file.write("\t\t" + line + "\n")
-            unit_test_file.write("\t} \n }")
+    lines_to_write = ['import static org.junit.jupiter.api.Assertions.*;\n',
+                    'import org.junit.jupiter.api.Test;\n',
+                    'import com.myapp.*;\n',
+                    'public class UnitTest {\n']
+
+    with open(pathToFile, "w+") as unit_test_file:
+        unit_test_file.writelines(lines_to_write)
+
+        for index, unit_test in enumerate(unit_tests_data):
+            for index_i, line in enumerate(unit_test.split('\n')):
+                unit_test_file.writelines(['\t@Test \n', 
+                                        '\tpublic void test' + str(index_i) + '() { \n'])
+                unit_test_file.write('\t\t' + line + '\n')
+                unit_test_file.write('\t} \n')                
+            unit_test_file.write('\n}')
             newUnitTest = UnitTest.objects.create(exercise=exercise, pathToFile=pathToFile, content=unit_test)
             newUnitTest.save()
 
